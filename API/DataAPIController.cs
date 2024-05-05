d55284a34b7a4b9a657aef2349a9ca0daf4a8e03
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-05-06 02:10:49 +0530

    Add project files.

---
+++ API/DataAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject1.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataAPIController : ControllerBase
+    {
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+using CoreProject1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreProject1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Students()
+        {
+            return View();
+        }
+
+        public IActionResult Teachers()
+        {
+            return View();
+        }
+
+        public IActionResult Notices()
+        {
+            return View();
+        }
+        public IActionResult Finance()
+        {
+            return View();
+        }
+
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+
+        public IActionResult Classes()
+        {
+            return View();
+        }
+
+        public IActionResult Departments()
+        {
+            return View();
+        }
+
+        public IActionResult ChartsViewStudent()
+        {
+            return View();
+        }  public IActionResult ChartsViewTeacher()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
