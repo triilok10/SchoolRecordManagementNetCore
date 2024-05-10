da1400f2b833458754e6b6dc85fb5bc31d2d5e06
triilok10 <ittiku3@gmail.com>
2024-05-10 07:44:55 +0530

    Commit to the Post API

--- API/DataAPIController.cs
+++ API/DataAPIController.cs
@@ -60,11 +60,49 @@ namespace CoreProject1.API
             }
             catch (Exception ex)
             {
-                
+
                 return StatusCode(500, "An error occurred while processing your request.");
             }
             return Ok(ltrStudents);
         }
-      
+
+        [HttpGet]
+        public IActionResult AddStudentAPI(Student pStudent)
+        {
+            try
+            {
+                
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("AddStudentMain", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Filepath", pStudent.Filepath);
+                        cmd.Parameters.AddWithValue("@FirstName", pStudent.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", string.IsNullOrEmpty(pStudent.LastName) ? (object)DBNull.Value : pStudent.LastName);
+                        cmd.Parameters.AddWithValue("@Class", pStudent.Class);
+                        cmd.Parameters.AddWithValue("@Gender", pStudent.Gender);
+                        cmd.Parameters.AddWithValue("@FatherName", pStudent.FatherName);
+                        cmd.Parameters.AddWithValue("@MotherName", pStudent.MotherName);
+                        cmd.Parameters.AddWithValue("@Address", pStudent.Address);
+                        cmd.Parameters.AddWithValue("@Remark", pStudent.Remarks);
+                        cmd.Parameters.AddWithValue("@Email", pStudent.Email);
+                        cmd.Parameters.AddWithValue("@Mobile", pStudent.Mobile);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                return StatusCode(500, "An error occurred while processing the request.");
+            }
+
+            return Ok(new { Message = "User Record " + pStudent.FirstName + " successfully saved!" });
+        }
+
+
     }
 }
--- Controllers/StudentUtilityController.cs
+++ Controllers/StudentUtilityController.cs
@@ -6,7 +6,9 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace CoreProject1.Controllers
 {
@@ -19,7 +21,7 @@ namespace CoreProject1.Controllers
         {
             _httpClient = httpClient;
             _baseUrl = "https://localhost:7054/";
-           
+
         }
 
         [HttpGet]
@@ -42,12 +44,66 @@ namespace CoreProject1.Controllers
             }
             catch (Exception ex)
             {
-                // Log the exception or handle it appropriately
                 return View("Error");
             }
         }
-   
-              
+
+        [HttpPost]
+        public async Task<IActionResult> CreateStudentData(Student pStudent, IFormFile File)
+        {
+            try
+            {
+                if (pStudent.FirstName != "" && pStudent.FatherName != "" && pStudent.Email != "")
+                {
+                    if (File != null && File.Length > 0)
+                    {
+                        string FileName = Path.GetFileName(File.FileName);
+                        string FilePathData = Path.Combine("wwwroot", "Images", "UserImages", FileName);
+                        using (var stream = new FileStream(FilePathData, FileMode.Create))
+                        {
+                            File.CopyTo(stream);
+                        }
+
+                        pStudent.Filepath = FileName;
+                    }
+                }
+
+                string Apiurl = _baseUrl + "api/DataAPI/AddStudentAPI";
+
+                string fullurl = $"{Apiurl}?FirstName={(string.IsNullOrWhiteSpace(pStudent.FirstName) ? "" : HttpUtility.UrlEncode(pStudent.FirstName))}" +
+                    $"&LastName={(string.IsNullOrWhiteSpace(pStudent.LastName) ? "" : HttpUtility.UrlEncode(pStudent.LastName))}" +
+                    $"&FatherName={(string.IsNullOrWhiteSpace(pStudent.FatherName) ? "" : HttpUtility.UrlEncode(pStudent.FatherName))}" +
+                    $"&MotherName={(string.IsNullOrWhiteSpace(pStudent.MotherName) ? "" : HttpUtility.UrlEncode(pStudent.MotherName))}" +
+                    $"&Mobile={(string.IsNullOrWhiteSpace(pStudent.Mobile) ? "" : HttpUtility.UrlEncode(pStudent.Mobile))}" +
+                    $"&Class={(string.IsNullOrWhiteSpace(pStudent.Class.ToString()) ? "" : HttpUtility.UrlEncode(pStudent.Class.ToString()))}" +
+                    $"&Gender={(string.IsNullOrWhiteSpace(pStudent.Gender.ToString()) ? "" : HttpUtility.UrlEncode(pStudent.Gender.ToString()))}" +
+                    $"&Address={(string.IsNullOrWhiteSpace(pStudent.Address) ? "" : HttpUtility.UrlEncode(pStudent.Address))}" +
+                    $"&Remarks={(string.IsNullOrWhiteSpace(pStudent.Remarks) ? "" : HttpUtility.UrlEncode(pStudent.Remarks))}" +
+                    $"&Filepath={(string.IsNullOrWhiteSpace(pStudent.Filepath) ? "" : HttpUtility.UrlEncode(pStudent.Filepath))}" +
+                    $"&Email={(string.IsNullOrWhiteSpace(pStudent.Email) ? "" : HttpUtility.UrlEncode(pStudent.Email))}";
+
+
+                HttpResponseMessage response = await _httpClient.GetAsync(fullurl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responsebody = await response.Content.ReadAsStringAsync();
+                    List<Student> objstudent = JsonConvert.DeserializeObject<List<Student>>(responsebody);
+                    return View(objstudent);
+                }
+                else
+                {
+
+                    return View("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+
+
         public IActionResult AddStudent()
         {
             return View();
